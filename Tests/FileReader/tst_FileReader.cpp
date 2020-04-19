d1a4154c1b885e48379bf6c7653f3190d9084070
Stan Morris <pixelgrease@gmail.com>
2020-04-18 23:13:33 -0700

    Correct issues in unit tests that prevent proper operation

--- Tests/FileReader/tst_FileReader.cpp
+++ Tests/FileReader/tst_FileReader.cpp
@@ -1,3 +1,28 @@
+/*  tst_FileReader.cpp
+
+    Unit tests for the FileReader class
+
+    Copyright (c) 2020 Stan Morris
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.
+*/
+
 #include "FileReader.h"
 #include <QSignalSpy>
 #include <QtTest>
@@ -71,7 +96,7 @@ void tst_FileReader::filename_member()
     fr.setFilename(FILENAME);
     QCOMPARE(fr.m_filename, FILENAME);
     QCOMPARE(fr.filename(), FILENAME);
-    QCOMPARE(fr.property("filename"), FILENAME);
+    QCOMPARE(fr.property("filename").toString(), FILENAME);
 }
 
 void tst_FileReader::isValid_with_invalid_folder()
--- Tests/FileWatcher/tst_FileWatcher.cpp
+++ Tests/FileWatcher/tst_FileWatcher.cpp
@@ -1,3 +1,28 @@
+/*  tst_FileWatcher.cpp
+
+    Unit tests for the FileWatcher class
+
+    Copyright (c) 2020 Stan Morris
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.
+*/
+
 #include "FileWatcher.h"
 #include <QSignalSpy>
 #include <QtTest>
@@ -76,7 +101,7 @@ void tst_FileWatcher::filename_member()
     fw.setFilename(FILENAME);
     QCOMPARE(fw.m_filename, FILENAME);
     QCOMPARE(fw.filename(), FILENAME);
-    QCOMPARE(fw.property("filename"), FILENAME);
+    QCOMPARE(fw.property("filename").toString(), FILENAME);
 }
 
 void tst_FileWatcher::filename_property()
@@ -87,7 +112,7 @@ void tst_FileWatcher::filename_property()
     fw.setProperty("filename", FILENAME);
     QCOMPARE(fw.m_filename, FILENAME);
     QCOMPARE(fw.filename(), FILENAME);
-    QCOMPARE(fw.property("filename"), FILENAME);
+    QCOMPARE(fw.property("filename").toString(), FILENAME);
 }
 
 void tst_FileWatcher::isValid_with_invalid_folder()
